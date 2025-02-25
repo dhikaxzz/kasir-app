222d7351e71bd8e16eee087edd23741ab06fd357
dhikaxzz <afdhikasyahputra@gmail.com>
2025-02-25 08:42:54 +0700

    update tampilan barangresource

--- app/Filament/Resources/BarangResource.php
+++ app/Filament/Resources/BarangResource.php
@@ -17,6 +17,8 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
 
 class BarangResource extends Resource
 {
@@ -27,69 +29,87 @@ class BarangResource extends Resource
     public static function form(Form $form): Form
     {
         return $form->schema([
-            TextInput::make('kode_barang')
-                ->unique(ignoreRecord: true)
-                ->required()
-                ->label('Kode Barang')
-                ->placeholder('Masukkan kode barang unik'),
-            
-            TextInput::make('nama_barang')
-                ->required()
-                ->label('Nama Barang')
-                ->placeholder('Masukkan nama barang'),
+            Section::make('Informasi Barang')
+                ->description('Masukkan detail barang yang akan dijual.')
+                ->schema([
+                    Grid::make(2)->schema([
+                        TextInput::make('kode_barang')
+                            ->label('Kode Barang')
+                            ->unique(ignoreRecord: true)
+                            ->required()
+                            ->placeholder('Masukkan kode unik'),
+                        
+                        TextInput::make('nama_barang')
+                            ->label('Nama Barang')
+                            ->required()
+                            ->placeholder('Masukkan nama barang'),
+                    ]),
+        
+                    Grid::make(2)->schema([
+                        Select::make('kategori')
+                            ->label('Kategori Barang')
+                            ->options([
+                                'Elektronik' => 'Elektronik',
+                                'Makanan' => 'Makanan',
+                                'Minuman' => 'Minuman',
+                                'Pakaian' => 'Pakaian',
+                                'Produk Rumah Tangga' => 'Produk Rumah Tangga',
+                                'Aksesoris' => 'Aksesoris',
+                                'Perlengkapan Sekolah' => 'Perlengkapan Sekolah',
+                            ])
+                            ->required(),
+        
+                        TextInput::make('merek')
+                            ->label('Merek')
+                            ->placeholder('Masukkan merek barang'),
+                    ]),
+        
+                    Textarea::make('deskripsi')
+                        ->label('Deskripsi Barang')
+                        ->placeholder('Tambahkan deskripsi barang')
+                        ->maxLength(255),
+                ]),
+        
+            Section::make('Detail & Harga')
+                ->description('Tambahkan informasi tambahan tentang barang.')
+                ->schema([
+                    Grid::make(2)->schema([
+                        Select::make('satuan')
+                            ->label('Satuan Barang')
+                            ->options([
+                                'pcs' => 'PCS',
+                                'kg' => 'KG',
+                                'liter' => 'LITER',
+                            ])
+                            ->required(),
+        
+                        TextInput::make('harga_jual')
+                            ->label('Harga Jual')
+                            ->prefix('Rp')
+                            ->numeric()
+                            ->required(),
+                    ]),
+        
+                    Grid::make(2)->schema([
+                        DatePicker::make('tanggal_kadaluarsa')
+                            ->label('Tanggal Kadaluarsa')
+                            ->required(),
 
-            Select::make('kategori')
-                ->label('Kategori')
-                ->options([
-                    'Elektronik' => 'Elektronik',
-                    'Makanan' => 'Makanan',
-                    'Minuman' => 'Minuman',
-                    'Produk' => 'Pakaian',
-                    'Produk Rumah Tangga' => 'Produk Rumah Tangga',
-                    'Aksesoris' => 'Aksesoris',
-                    'Perlengkapan Sekolah' => 'Perlengkapan Sekolah',
-                ])
-                ->required(),
-
-            TextInput::make('merek')
-                ->label('Merek')
-                ->placeholder('Masukkan merek barang'),
-
-            Textarea::make('deskripsi')
-                ->label('Deskripsi')
-                ->placeholder('Deskripsi barang'),
-
-            Select::make('satuan')
-                ->label('Satuan')
-                ->options([
-                    'pcs' => 'PCS',
-                    'kg' => 'KG',
-                    'liter' => 'LITER',
-                ])
-                ->required(),
-
-            TextInput::make('harga_jual')
-                ->label('Harga Jual')
-                ->prefix('Rp')
-                ->numeric()
-                ->required(),
-
-            TextInput::make('lokasi_barang')
-                ->label('Lokasi Barang')
-                ->placeholder('Rak, gudang, dll.'),
-
-            DatePicker::make('tanggal_kadaluarsa')
-                ->label('Tanggal Kadaluarsa')
-                ->required(),
-
-            Select::make('status')
-                ->label('Status Barang')
-                ->options([
-                    'aktif' => 'Aktif',
-                    'nonaktif' => 'Nonaktif',
-                ])
-                ->required(),
-        ]);
+                        TextInput::make('lokasi_barang')
+                            ->label('Lokasi Barang')
+                            ->placeholder('Misalnya: Rak A, Gudang, dll.'),
+        
+                    ]),
+        
+                    Select::make('status')
+                        ->label('Status Barang')
+                        ->options([
+                            'aktif' => 'Aktif',
+                            'nonaktif' => 'Nonaktif',
+                        ])
+                        ->required(),
+                ]),
+        ]);        
     }
 
     public static function table(Table $table): Table
@@ -110,11 +130,10 @@ class BarangResource extends Resource
 
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 

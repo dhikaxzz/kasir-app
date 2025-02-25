6b18d18720c6c2024c6d4a9c70efa60ca556fb7b
dhikaxzz <afdhikasyahputra@gmail.com>
2025-02-25 08:47:09 +0700

    update tampilan pelanggan

--- app/Filament/Resources/PelangganResource.php
+++ app/Filament/Resources/PelangganResource.php
@@ -15,6 +15,8 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
 
 class PelangganResource extends Resource
 {
@@ -24,13 +26,37 @@ class PelangganResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                TextInput::make('nama')->required()->label('Nama Pelanggan'),
-                TextInput::make('no_telpon')->tel()->label('Nomor Telepon'),
-                TextInput::make('email')->email()->label('Email'),
-                Textarea::make('alamat')->label('Alamat'),
-            ]);
+        return $form->schema([
+            Section::make('Informasi Pelanggan')
+                ->description('Masukkan data pelanggan.')
+                ->schema([
+                    Grid::make(2)->schema([
+                        TextInput::make('nama')
+                            ->label('Nama Pelanggan')
+                            ->required()
+                            ->placeholder('Masukkan nama pelanggan'),
+
+                        TextInput::make('no_telpon')
+                            ->label('No. Telepon')
+                            ->tel()
+                            ->required()
+                            ->placeholder('Masukkan nomor telepon')
+                            ->unique(ignoreRecord: true),
+                    ]),
+
+                    Grid::make(1)->schema([
+                        TextInput::make('email')
+                            ->label('Email')
+                            ->email()
+                            ->placeholder('Masukkan email pelanggan')
+                            ->unique(ignoreRecord: true),
+                        
+                        TextInput::make('alamat')
+                            ->label('Alamat')
+                            ->placeholder('Masukkan alamat pelanggan'),
+                    ]),
+                ]),
+        ]);
     }
 
     public static function table(Table $table): Table
@@ -38,19 +64,19 @@ class PelangganResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('nama')->searchable()->label('Nama Pelanggan'),
-                TextColumn::make('no_telpon')->label('Nomor Telepon'),
-                TextColumn::make('email')->label('Email'),
-                TextColumn::make('alamat')->truncate(50)->label('Alamat'),
+                TextColumn::make('no_telpon')->label('No. Telepon')->searchable(),
+                TextColumn::make('email')->label('Email')->searchable(),
+                TextColumn::make('alamat')->searchable()->label('Alamat')->limit(50),
             ])
             ->filters([])
-            
+
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
+            
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
